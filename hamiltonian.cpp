e9106c19267b7d6b494cf916fa0637ae97a805f5
cja14 <cja14@ic.ac.uk>
2015-01-27 17:55:33 +0000

    Updated hamiltonian to basic form

---
+++ hamiltonian.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+double get_hijab(int a, int b, std::vector<double> d);
+
+// Takes 4 arguments: type, posx, posy, posz
+int main(int argc, char **argv) { 
+  int i, j, a, b; // i,j loop over atoms; a,b loop over orbitals
+
+}
