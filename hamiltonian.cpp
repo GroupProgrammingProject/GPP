5a6d82b007ea77f70d21cac9bc0ad71237e5c05c
cja14 <cja14@ic.ac.uk>
2015-01-27 17:59:24 +0000

    little changed

--- hamiltonian.cpp
+++ hamiltonian.cpp
@@ -3,7 +3,7 @@
 
 double get_hijab(int a, int b, std::vector<double> d);
 
-// Takes 4 arguments: type, posx, posy, posz
+// Takes 4 vector arguments: type, posx, posy, posz
 int main(int argc, char **argv) { 
   int i, j, a, b; // i,j loop over atoms; a,b loop over orbitals
 
