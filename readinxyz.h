0a57ab7a95bdb74d41e1e2282e104aa8e241aa9b
jonasverschueren <jonas.verschueren10@imperial.ac.uk>
2015-01-28 14:22:33 +0000

    Started on the reading in files

--- readinxyz.h
+++ readinxyz.h
@@ -1,12 +1,12 @@
 // Read in from file functionality
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 
 // Reads in 4 vectors from cell file
-void ReadInXYZ(std::string filename, std::vector<std::string>* names, std::vector<double> x, std::vector<double> y, std::vector<double> z){
-	ifstream infile;
+void ReadInXYZ(char* filename, std::vector<std::string>* names, std::vector<double> x, std::vector<double> y, std::vector<double> z){
+	std::ifstream infile;
 	infile.open(filename);
-	string textarray[10];
-	for ()
+	std::string textarray[10];
 }
