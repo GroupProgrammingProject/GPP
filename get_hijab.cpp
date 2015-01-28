dbb98cf4260477b1b770fdfae483f6409a5c6e95
alisevii <alise.virbule@gmail.com>
2015-01-28 14:16:15 +0000

    updated Gethijab.h so that it does not take r modulus as input

--- get_hijab.cpp
+++ get_hijab.cpp
@@ -3,17 +3,16 @@
 #include <cmath>
 #include <string>
 
-double Gethijab(int i, int j,int a, int b, double* d,double r,int typei,int typej);
+double Gethijab(int i, int j,int a, int b, double* d,int typei,int typej);
 
 //std::string pointer
 //const std::string& name
 
 //Gethijab() returns value of non-scaled Hamiltionian matrix element h
-double Gethijab(int i, int j,int a, int b, double* d,double r,int typei,int typej){
+double Gethijab(int i, int j,int a, int b, double* d,int typei,int typej){
 
 int k; //for looping
 double h,Es,Ep,V[4];														//h,Es,Ep and V[4] is only used locally in Gethijab()
-double r_cutoff=2.6;														//where should I take r_cutoff from?
 double Es_C=-2.99,Ep_C=3.71;											//C orbital energies Es and Ep
 double V_CC[4],V_CH[4],V_HH[4];
 V_CC[0]=-5;V_CC[1]=4.7;V_CC[2]=5.5;V_CC[3]=-1.55;				//CC interaction 0=ss_sigma, 1=sp_sigma, 2=pp_sigma, 3=pp_pi
@@ -24,8 +23,7 @@ if(typei==6 && typej==6){												//add more if statements, when we have inte
 }
 
 //start V&G routine
-if(r>r_cutoff){h=0;}														//check if r within cut-off radius
-else if(i==j){
+if(i==j){
 	if(a==b){
 		if(a==0){h=Es;}
 		else{h=Ep;}
@@ -49,7 +47,7 @@ int main() {	//main does what hamiltonian.cpp would ask of Gethijab()
 
 //input from hamiltonian to Gethijab()
 int k,i,j,a,b;
-double h,d[3],r=1.73;
+double h,d[3];
 int typei=6,typej=6;
 for(k=0;k<3;k++){d[k]=1;}	//dummy d vector
 //end listing input
@@ -59,7 +57,7 @@ for(i=0;i<2;i++){
 		std::cout << "i and j are: " << i << " " << j << std::endl;
 		for(a=0;a<4;a++){
 			for(b=0;b<4;b++){
-				h=Gethijab(i,j,a,b,d,r,typei,typej);
+				h=Gethijab(i,j,a,b,d,typei,typej);
 				std::cout << h << "	";
 			} //end b loop
 			std::cout << std::endl;
