da36833259d1566cc3f94402485ceb6172dbac5a
cja14 <cja14@ic.ac.uk>
2015-01-28 15:13:44 +0000

    First "working" version of hamiltonian.h, included vect_test.cpp as a test main and S.h as a dummy scaling function.

--- hamiltonian.h
+++ hamiltonian.h
@@ -5,13 +5,6 @@
 #include "Gethijab.h"
 #include "S.h"
 
-/*double Gethijab(int i, int j, int a, int b, double* d, double r, int typei, int typej);
-  double S(double r);*/
-
-int Test(int n, std::vector<double>* posx) {
-  return 1;
-}
-
 // Takes 1 int and 4 vector arguments: n, type, posx, posy, posz
 void Hamiltonian(int n, std::vector<int>* type, std::vector<double>* posx, std::vector<double>* posy, std::vector<double>* posz){
   int i, j, a, b;                                       // i,j loop over atoms; a,b loop over orbitals
@@ -20,15 +13,11 @@ void Hamiltonian(int n, std::vector<int>* type, std::vector<double>* posx, std::
   double hijab;
   int  typei, typej;
   std::vector<double> H_MD(pow(4*n,2));                 // A matrix to be populated with interaction parameters for MD
-  typedef Eigen::Matrix<std::complex<double>, Eigen::Dynamic, Eigen::Dynamic> MatrixXd;
+  typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> MatrixXd;
   MatrixXd Hijab(4*n,4*n);
 
-  std::cout << "Checkpoint A" << std::endl;
-
   for (i=0;i<n;i++) {                                   // Cycle through atoms i
-    std::cout << "i = " << i << std::endl;
-    for (j=0;j<i;j++) {                                 // Cycle through atoms j (note upper triangular matrix)
-      std::cout << "j = " << j << std::endl;
+    for (j=0;j<=i;j++) {                                // Cycle through atoms j (note upper triangular matrix)
       typei = (*type).at(i);
       typej = (*type).at(j);      
       d[0]  = (*posx).at(i)-(*posx).at(j);
@@ -38,19 +27,15 @@ void Hamiltonian(int n, std::vector<int>* type, std::vector<double>* posx, std::
       sr    = S(r);                                     // Scaling parameter
       for (a=0;a<4;a++) {
 	for (b=0;b<4;b++) {
-	  std::cout << "hijab = h" << i << j << a << b << std::endl;
-	  hijab = Gethijab(i,j,a,b,d,r,typei,typej);    // Hamiltonian elements of ij interaction
+	  hijab = Gethijab(i,j,a,b,d,typei,typej);      // Hamiltonian elements of ij interaction
 	  H_MD.at((4*i+a)*4*j+b) = hijab;               // Vector of interactions to pass to MD
 	  H_MD.at((4*j+a)*4*i+b) = hijab;               // Vector of interactions to pass to MD
-	  std::cout << "hijab = " << hijab << std::endl;
 	  Hijab(4*i+a,4*j+b)     = sr*hijab;            // Scale hijab and populate matrix Hijab
 	  Hijab(4*j+a,4*i+b)     = sr*hijab;            // Scale hijab and populate matrix Hijab
 	}                                               // End loop over b
       }                                                 // End loop over a
-    }                                                   // End loop over i
-  }                                                     // End loop over j
-
-  std::cout << "Checkpoint B" << std::endl;
+    }                                                   // End loop over j
+  }                                                     // End loop over i
 
   std::cout << Hijab << std::endl;
 
--- vect_test.cpp
+++ vect_test.cpp
@@ -3,8 +3,6 @@
 #include <cmath>
 #include "hamiltonian.h"
 
-/*void Hamiltonian(int n, std::vector<int>* type, std::vector<double>* posx, std::vector<double>* posy, std::vector<double>* posz);*/
-
 int main() {
   std::vector<int> type(2);
   std::vector<double> posx(2);
@@ -20,17 +18,7 @@ int main() {
   posz.at(0) = 0.0000;
   posz.at(1) = 0.0000;
 
-  std::cout << "Checkpoint 0" << std::endl;
-
   Hamiltonian(n, &type, &posx, &posy, &posz);
-  //Test(n, &posx);
 
-  /*H_MD.at(0) = 0.00382;
-  H_MD.at(1) = 2.2398;
-  H_MD.at(2) = 4.00382;
-  double var = H_MD[0];
-  std::cout << "var = " << var << std::endl;
-  double var2 = H_MD.at(2);
-  std::cout << "var2 = " << var2 << std::endl;*/
   return 0;
 }
