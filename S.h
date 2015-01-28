cb3a789fe61129745170773912206da3244ab8f2
cja14 <cja14@ic.ac.uk>
2015-01-28 16:24:36 +0000

    Updated S.h to be functional

--- S.h
+++ S.h
@@ -1,4 +1,17 @@
 double S(double r, int typei, int typej) {
-  double s = 1;
-  return s;
+  double r0=1.536329; //nearest-neighbour atomic separation
+  double n=2;
+  double nc=6.5;
+  double rc=2.18;
+  double r1=2.45;
+  double S;
+  //constants 
+
+  if (r<r1){
+    S=pow(r0/r,n)*exp(n*(-pow(r/rc,nc)+pow(r0/rc,nc)));
+  }
+  else{
+    S=ts(r);
+  }
+  return S;
 }
