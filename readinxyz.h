d9f3509b3c668ac0afcd07c68590c5900077ab19
jonasverschueren <jonas.verschueren10@imperial.ac.uk>
2015-01-28 15:57:51 +0000

    We can now read in files from .xyz files

--- readinxyz.h
+++ readinxyz.h
@@ -2,11 +2,38 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 #include <vector>
+#include <iomanip>
 
-// Reads in 4 vectors from cell file
-void ReadInXYZ(char* filename, std::vector<std::string>* names, std::vector<double> x, std::vector<double> y, std::vector<double> z){
-	std::ifstream infile;
-	infile.open(filename);
-	std::string textarray[10];
+void ReadInXYZ (char* filename, std::vector<int>* atomtypes, std::vector<double>* x, std::vector<double>* y, std::vector<double>* z);
+std::map<std::string, int> SetElementMap();
+
+// Reads in 4 vectors from cell file: elements, x , y and z coords
+void ReadInXYZ(char* filename, std::vector<int>* atomtypes, std::vector<double>* xvect, std::vector<double>* yvect, std::vector<double>* zvect){
+	std::ifstream infile(filename);
+	// Initiate the map of elements (symbol-> atomic number)
+	std::map<std::string, int> elmap=SetElementMap();
+	// First two lines are handled seperately and are not to be stored
+	int num;
+	char skip[2];
+	infile>>num;
+	infile>>skip>>skip>>skip>>skip;
+	// Store the molecule type and x, y, z positions
+	double x, y, z;
+	std::string type;
+	while (infile>>type>>x>>y>>z){
+		atomtypes->push_back(elmap[type]);	
+		xvect->push_back(x);
+		yvect->push_back(y);
+		zvect->push_back(z);
+	}
+}
+
+// Function to initiate a map to translate element symbol to integer values corresponding to its atomic number
+std::map<std::string, int> SetElementMap(){
+	std::map<std::string, int> map;
+	map["H"]=1;
+	map["C"]=6;
+	return map;
 }
