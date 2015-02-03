eab6ef6fb7ff9c28510bbadeb483efe5e9229941
alisevii <alise.virbule@gmail.com>
2015-02-03 19:19:33 +0000

    TB is now working in the steer.cpp file

--- steer.cpp
+++ steer.cpp
@@ -13,25 +13,21 @@ int main(int argc, char* argv[]){
 	// Read in types, 
 	std::vector<int> type;
 	std::vector<double> posx, posy, posz;
-	std::vector<int>* typeptr = &type;
-	std::vector<double>* Xptr = &posx;
-	std::vector<double>* Yptr = &posy;
-	std::vector<double>* Zptr = &posz;
-	ReadInXYZ (argv[1], typeptr, Xptr, Yptr, Zptr);
-// Debug output
-/*Print (typeptr);
-Print (Xptr);
-Print (Yptr);
-Print (Zptr);
-*/
-	double ebs;
+	ReadInXYZ (argv[1], &type, &posx, &posy, &posz);
 	// Number of atoms
 	int n=type.size();
-	Matrix Hamiltonian;
-	Matrix* Hamiltonianptr=&Hamiltonian;
+	// Create empty arrays needed for MD
+	std::vector<double> H_MD(16*n*n),eigvects(16*n*n);
+	// Energies from TB model
+	double ebs,erep,etot;
 
-	ebs=Hamiltonian(n,typeptr,Xptr,Yptr,Zptr);
-	std::cout << "Ebs=" << ebs << " eV" << std::endl;
+	// Starting TB	module: calculating energies
+	ebs=Hamiltonian(n,&type,&posx,&posy,&posz,&H_MD,&eigvects);
+	//H_MD and eigvects have now also been populated
+	erep=Erep(type,posx,posy,posz);
+// Determining erep works, however, we need to check if we're passing pointers or arrays to Erep()
+	etot=ebs+erep;
 	
+
 return 0;
 }
