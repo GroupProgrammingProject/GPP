a1f8a53f37cc1790b07ad18a8abeac79210460bf
jonasverschueren <jonas.verschueren10@imperial.ac.uk>
2015-02-03 17:47:30 +0000

    Slight modifications

--- steer.cpp
+++ steer.cpp
@@ -1,26 +1,29 @@
+// File that assembles all the different TB and MD modules
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include "Eigen/Dense"
 #include "readinxyz.h"
 #include "vectorfunctions.h"
+#include "hamiltonian.h"
+#include "functions.h"
+#include "Gethijab.h"
+#include "MolDyn.h"
 
 int main(int argc, char* argv[]){
+	// Read in types, 
 	std::vector<int> types;
 	std::vector<double> X, Y, Z;
 	std::vector<int>* typeptr = &types;
 	std::vector<double>* Xptr = &X;
 	std::vector<double>* Yptr = &Y;
 	std::vector<double>* Zptr = &Z;
-	
-	//Read in values from specified .xyz file
 	ReadInXYZ (argv[1], typeptr, Xptr, Yptr, Zptr);
-	Print (typeptr);
-	Print (Xptr);
-	Print (Yptr);
-	Print (Zptr);
-	
-	const char* filename="testz.txt";
-	Print(Zptr, filename);
+// Debug output
+Print (typeptr);
+Print (Xptr);
+Print (Yptr);
+Print (Zptr);
 	
 return 0;
 }
