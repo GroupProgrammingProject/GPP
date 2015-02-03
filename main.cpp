60961c2591aa258153167dd1a8c02b07674a1c9d
jgolebiowski <jacek.golebiowski14@imperial.ac.uk>
2015-02-03 15:17:04 +0000

    2nd version of repulsive energy function, possibly working this time

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@
 using namespace std;
 
 int main() {
- 
-  hello();
+
   double sos;
 
   /*ofstream tm;
@@ -34,6 +33,20 @@ int main() {
   for(int i=0;i<10;i++){
     wekt.push_back(3.32+i*0.04);
   }
+
+  std::vector<double> rx,ry,rz;
+  std::vector<int> type;
+  for(int i=0;i<2;i++){
+    rx.push_back(i*0.5);
+    ry.push_back(i*0.5);
+    rz.push_back(i*0.5);
+    type.push_back(6);
+  }
+  test(rx);
+  test(ry);
+  test(rz);
   test(wekt);
+  double erep=Erep(type,rx,ry,rz);
+  cout<<"e rep: "<<erep<<endl;
 
 };
