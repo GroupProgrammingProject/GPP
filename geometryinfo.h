5c19f4b6c9b9e1cf450782be83678d695abedefb
jonasverschueren <jonas.verschueren10@imperial.ac.uk>
2015-02-11 19:52:59 +0000

    Added routine to decide if nearest neighbours list needs to be recalculated

--- geometryinfo.h
+++ geometryinfo.h
@@ -77,6 +77,43 @@ void NearestNeighbours(Eigen::MatrixXi* inear, std::vector<int>* nnear, Eigen::M
 }
 
 
+// Simple absolute value function
+double Abs(double c){
+	double result=c;
+	if (c<0){result=-c;}
+	else{;}
+return result;
+}
+
+// Function to set all elements of 2 equal length vectors equal too each other
+// equivalent to A=B for std::vector<double> objects
+void SetEqual(std::vector<double>* a, std::vector<double>* b){
+	int n=a->size();
+	for (int i=0; i<n; i++){a->at(i)=b->at(i);}
+}
+
+// Function that decides whether the Nearest neighbours lists need to be recalculated between MD steps
+bool RecalculateNearestNeighbours(std::vector<double>* refposx, std::vector<double>* refposy, std::vector<double>* refposz, std::vector<double>* posx, std::vector<double>* posy, std::vector<double>* posz, double rc, double rv){
+	int numatoms=posx->size();
+	bool recalc=0;
+	std::vector<double> disp(numatoms);
+	for (int i=0; i<numatoms; i++){
+		double xdisp=posx->at(i)-refposx->at(i);
+		double ydisp=posy->at(i)-refposy->at(i);
+		double zdisp=posz->at(i)-refposz->at(i);
+		disp.at(i)=sqrt(xdisp*xdisp+ydisp*ydisp+zdisp*zdisp);
+	}
+	double dmax=*(max_element(disp.begin(), disp.end()));
+std::cout<<dmax<<std::endl;
+	if (dmax>0.4*Abs(rc-rv)){
+		SetEqual(posx, refposx);
+		SetEqual(posy, refposy);
+		SetEqual(posz, refposz);
+		recalc=1;
+	}
+	else{;}
+return recalc;
+}
 
 
 #endif
--- testmain.cpp
+++ testmain.cpp
@@ -11,20 +11,20 @@
 int main(int argc, char* argv[]){
 	if (argc!=2){std::cout<<"You should append one and only one xyz file to the main!!"<<std::endl;}
 	// Read in types, 
-	std::vector<int> type;
 	std::vector<double> posx, posy, posz;
-	ReadInXYZ (argv[1], &type, &posx, &posy, &posz);
+	ReadInXYZ (argv[1], &posx, &posy, &posz);
 
 	// Test distance function
-	const int N=type.size();
+	const int N=posx.size();
 	Eigen::MatrixXd rx (N, N);
 	Eigen::MatrixXd ry (N, N);
 	Eigen::MatrixXd rz (N, N);
 	Eigen::MatrixXd modr (N, N);
 	std::vector<int>nnear(N);
 	Eigen::MatrixXi inear = Eigen::MatrixXi::Constant(N, 5, -123456);					// Random number to indicate that a value has been unassigned
-	GetAllDistances(&modr, &rx, &ry, &rz, &posx, &posy, &posz, 4);
-	NearestNeighbours(&inear, &nnear, &modr, 4);
+	double rc=2.6, rv=4;
+	GetAllDistances(&modr, &rx, &ry, &rz, &posx, &posy, &posz, rc);
+	NearestNeighbours(&inear, &nnear, &modr, rv);
 	
 std::cout<<rx<<std::endl;
 std::cout<<ry<<std::endl;
@@ -32,14 +32,16 @@ std::cout<<rz<<std::endl;
 std::cout<<modr<<std::endl;
 Print(&nnear);
 std::cout<<inear<<std::endl;
-
-
-//	std::vector<double> modr1(N*N), modr2(N*N), rx(N*N), ry(N*N), rz(N*N) ;
-//	GetDistanceComponents(&rx,&ry, &rz, &posx, &posy, &posz);
-//	GetAllDistances(&modr1, &rx, &ry, &rz, &posx, &posy, &posz);
-//	GetAllDistances(&modr2, &rx, &ry, &rz, &posx, &posy, &posz, rc);
-
-//	NearestNeighbours (&inear, &nnear, &modr1, 4.);
 	
+	std::vector<double>refposx(N);
+	for (int i=0; i<N; i++){refposx.at(i)=posx.at(i)+.57;}
+Print(&refposx);
+	
+	bool recalculate=RecalculateNearestNeighbours(&refposx, &posy, &posz, &posx, &posy, &posz, rc, rv);
+
+Print(&posx);	
+	std::cout<<"Recalculate: "<<recalculate<<std::endl;
+	
+
 return 0;
 }
