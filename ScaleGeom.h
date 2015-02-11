e3a81131d4244c3c9f3b6bf4786f5edd5811e55e
cja14 <cja14@ic.ac.uk>
2015-02-11 17:42:37 +0000

    Added ScaledGeom.h which manipulates atom positions.

---
+++ ScaleGeom.h
@@ -0,0 +1,20 @@
+#include <cmath>
+
+void ScaleGeom(int n, double ascale, double bscale, double cscale, std::vector<double>* posx, std::vector<double>* posy, std::vector<double>* posz) {
+  for (int i=0;i<n;i++) {
+	 (*posx).at(i) = ascale*(*posx).at(i);
+	 (*posy).at(i) = bscale*(*posy).at(i);
+	 (*posz).at(i) = cscale*(*posz).at(i);
+  }
+}
+
+void ScaleCell(int n, int ascale, int bscale, int cscale, double a, double b, double c, std::vector<double>* posx, std::vector<double>* posy, std::vector<double>* posz) {
+  for (int i=0;i<n;i++) {
+	 for (int j=0;j<ascale;j++) {
+		(*posx).at(i) = j*a + (*posx).at(i);}
+	 for (int j=0;j<bscale;j++) {
+		(*posy).at(i) = j*b + (*posy).at(i);}
+	 for (int j=0;j<cscale;j++) {
+		(*posz).at(i) = j*c + (*posz).at(i);}
+  }
+}
