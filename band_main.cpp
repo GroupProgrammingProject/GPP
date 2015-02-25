0fbf020f7aad8d4966cad4897a7070126b08f731
jgolebiowski <jacek.golebiowski14@imperial.ac.uk>
2015-02-25 11:28:36 +0000

    anged the name of geo main, removed old band main

--- band_main.cpp
+++ band_main.cpp
@@ -4,62 +4,105 @@
 #include "Eigen/Dense"
 #include "readinxyz.h"
 #include "vectorfunctions.h"
+#include "band_hamiltonian.h"
 #include "functions.h"
 #include "geometryinfo.h"
-#include "band_hamiltonian.h"
 
 int main(int argc, char* argv[]){
 	if (argc<1){std::cout<<"You should append a file to the main object!"<<std::endl;}
 	if (argc!=2){std::cout<<"You should append one and only one xyz file to the main!!"<<std::endl;}
 	
 	// Turn verbose mode (hamiltonian routine) on/off
-	bool v=1;
+	bool v=0;
 	// Read in types, 
-	std::vector<int> type;
 	std::vector<double> posx, posy, posz;
-	ReadInXYZ (argv[1], &type, &posx, &posy, &posz);
+	ReadInXYZ (argv[1], &posx, &posy, &posz);
 	// Number of atoms
 	int n=posx.size();
-	// Calculate distances
+	std::cout << "n = " <<n <<std::endl;
+	std::vector<double> posxnew(n), posynew(n), posznew(n);
 	Eigen::MatrixXd modr(n,n);
 	Eigen::MatrixXd rx(n,n);
 	Eigen::MatrixXd ry(n,n);
 	Eigen::MatrixXd rz(n,n);
-//	std::vector<double> modr(n*n),rx(n*n),ry(n*n),rz(n*n);
-	GetAllDistances(&modr,&rx,&ry,&rz,&posx,&posy,&posz);
-	// Create empty arrays needed for MD
-//	std::vector<double> eigvects(16*n*n);
+	double ebs = 0,erep = 0,etot = 0;
+
+	// Change if using PBCs
+	double a =13.1;
+	double b =10;
+	double c =10;
+	double rv = 2.98;
+
 	Eigen::MatrixXd eigvects(4*n,4*n);
-	// Energies from TB model
-	double ebs,erep,etot;
+
+	// Calculate distances
+	PbcGetAllDistances(&modr,&rx,&ry,&rz,&posx,&posy,&posz,a,b,c,rv);
+	int kn = 50; // Number of kpts 
 
 	//k used for the band structure 
-	double k=0;
+	//double k=0;
 	// Starting TB	module: calculating energies
 	std::vector<double> eigenvalaar (4*n);
 
 	std::ofstream band;
-	band.open ("band_structure.dat");
-	double K_MAX=M_PI/1.3;
-	double K_STEP=2*M_PI/(1.3*50);
-	for(k=0;k<=K_MAX;k=k+K_STEP){
-	ebs=band_Hamiltonian(n,&modr,&rx,&ry,&rz,&eigvects,&eigenvalaar,k,v);
-	//H_MD and eigvects have now also been populated
-	band<<k<<"\t";
-	for(int i=0;i<10;i++){
-	  band<<eigenvalaar[i]<<"\t";
-	}
-	band<<"\n";
+	band.open ("band_structure6.dat");
+	double K_MAX=M_PI/a;  //(a/n);
+	double K_STEP=2*M_PI/(a*kn);        // 2*M_PI/((a/n)*50);
+	double BZ = M_PI/(a/n);
+	double kprim;
+	//ebs=band_Hamiltonian(n,&modr,&rx,&ry,&rz,&eigvects,&eigenvalaar,0,v);
+	for(double k=0;k<=K_MAX;k=k+K_STEP){
+	  ebs=ebs+(1.0/(double)kn)*band_Hamiltonian(n,&modr,&rx,&ry,&rz,&eigvects,&eigenvalaar,k,v);
+		//H_MD and eigvects have now also been populated
+		//band<<k<<"\t";
+		for(int i=0;i<4*n;i++){
+
+		  // BZ band 0
+		  if (i/2-(double)i/2.0 == 0 && i < n) {             // If even band
+			 kprim = fmod(i*K_MAX + k,BZ);
+		  }
+		  else if (i/2-(double)i/2.0 != 0 && i < n) {                                    // If odd band
+			 kprim = fmod(i*K_MAX + (K_MAX-k),BZ);
+		  }
+
+		  // BZ band 1
+		  else if (i/2-(double)i/2.0 == 0 && i < 2*n) {             // If even band
+			 kprim = BZ - (i%n)*K_MAX - k;
+		  }
+		  else if (i/2-(double)i/2.0 != 0 && i < 2*n) {                                    // If odd band
+			 kprim = BZ - (i%n)*K_MAX - (K_MAX - k);
+		  }
+
+		  // BZ band 2
+		  if (i/2-(double)i/2.0 == 0 && i < 3*n) {             // If even band
+			 kprim = fmod((i-2*n)*K_MAX + k,BZ);
+		  }
+		  else if (i/2-(double)i/2.0 != 0 && i < 3*n) {                                    // If odd band
+			 kprim = fmod((i-2*n)*K_MAX + (K_MAX-k),BZ);
+		  }
+
+		  // Else
+		  else if (i/2-(double)i/2.0 == 0 && i > n*2) {             // If even band
+			 kprim = fmod(i*K_MAX + k,BZ);
+		  }
+		  else if (i/2-(double)i/2.0 != 0 && i > n*2) {                                    // If odd band
+			 kprim = fmod(i*K_MAX + (K_MAX-k),BZ);
+		  }
+
+		  band<<kprim<<"\t";
+		  band<<eigenvalaar[i]<<"\n";
+		}
+		//band<<"\n";
 	}
 	band.close();
-
-
+	//H_MD and eigvects have now also been populated
 	erep=Erep(&modr);
+	// Determining erep works, however, we need to check if we're passing pointers or arrays to Erep()
 	etot=ebs+erep;
 
 	std::cout << "Ebs = " << ebs << std::endl;
 	std::cout << "Erep = " << erep << std::endl;
 	std::cout << "Etot = " << etot << std::endl;
 
-return 0;
+	return 0;
 }
