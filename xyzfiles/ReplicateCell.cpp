b654a068dbb1f147cf6073a4ca0a6c9f3e2a22bb
cja14 <cja14@ic.ac.uk>
2015-03-01 09:48:41 +0000

    Added working versions of ReplicateCell.cpp and ScaleCell.cpp

--- xyzfiles/ReplicateCell.cpp
+++ xyzfiles/ReplicateCell.cpp
@@ -2,34 +2,36 @@
 #include <fstream>
 #include <vector>
 #include <iomanip>
-#include "readinxyz.h"
+#include <stdlib.h>
+#include "../readinxyz.h"
 #include "ScaleGeom.h"
 
+// Takes "input.xyz output.xyz A B C" as input where A, B and C are the integers by which you wish to replicate the input.xyz structure
+
 int main(int argc, char* argv[]){
-  if (argc<2){std::cout<<"You should append two files to the main object!"<<std::endl;}
-  if (argc!=3){std::cout<<"You should append one input and one output xyz files to the main!!"<<std::endl;}
+  if (argc<5){std::cout<<"You should append two files and the numbers by which you wish to replicate the cell along each axis to the main object!"<<std::endl;}
+  if (argc!=6){std::cout<<"You should append one input and one output xyz files and the numbers by which you wish to replicate the cell along each axis to the main!!"<<std::endl;}
   
+  bool pbc = 1;
+  std::vector<double> lats(3);
   std::vector<double> posx, posy, posz;
-  ReadInXYZ (argv[1], &posx, &posy, &posz);
+  ReadInXYZ (argv[1], &posx, &posy, &posz, &lats, pbc);
   // Number of atoms
   int n=posx.size();
 
   // Replicate cell in n dimensions
-  double ascale = 3;
-  double bscale = 2;
-  double cscale = 1;
-  int n_new = ascale*bscale*cscale*n;
-  double a = 2.4595121;
-  double b = 4.26000;               
-  double c = 6;
-  std::vector<double> posxnew(n_new), posynew(n_new), posznew(n_new); 
-  ScaleCell(n, ascale, bscale, cscale, a, b, c, &posx, &posy, &posz, &posxnew, &posynew, &posznew);
-  
+  int scale[3] = {atoi(argv[3]),atoi(argv[4]),atoi(argv[5])};
+  std::cout << "scale: " << scale[0] << " " << scale[1] << " " << scale[2] << std::endl;
+  int n_new = scale[0]*scale[1]*scale[2]*n;
+  std::cout << "n_new = " << n_new << std::endl;
+  std::vector<double> posxnew(n_new), posynew(n_new), posznew(n_new), latsnew(3); 
+  ScaleCell(n, scale, &lats, &posx, &posy, &posz, &posxnew, &posynew, &posznew, &latsnew, pbc);
+
   // Print output .xyz cell
   std::string outputfile = argv[2];
   std::ofstream cell(outputfile.c_str());
   cell << n_new << "\n";
-  cell << outputfile << "\t" << ascale*a << "\t" << bscale*b << "\t" << cscale*c << "\n";
+  cell << outputfile << "\t" << latsnew[0] << "\t" << latsnew[1] << "\t" << latsnew[2] << "\n";
   for (int j=0;j<n_new;j++){
 	 cell << "C\t" << std::setprecision(6) << posxnew.at(j) << "\t" << std::setprecision(6) << posynew.at(j) <<  "\t" << std::setprecision(6) << posznew.at(j) << "\n";
   }
