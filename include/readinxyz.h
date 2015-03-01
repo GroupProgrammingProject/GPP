6b2af443e3ad44e2cc72d96292ab3fd1e590b2cd
cja14 <cja14@ic.ac.uk>
2015-03-01 10:49:06 +0000

    Added gen_kpath.cpp for generating linear paths of kpoints in kspace

--- include/readinxyz.h
+++ include/readinxyz.h
@@ -8,7 +8,7 @@
 #include <map>
 #include <vector>
 
-void ReadInXYZ (char* filename, std::vector<double>* x, std::vector<double>* y, std::vector<double>* z);
+void ReadInXYZ (char* filename, std::vector<double>* x, std::vector<double>* y, std::vector<double>* z, bool pbc);
 std::map<std::string, int> SetElementMap();
 
 // Function to initiate a map to translate element symbol to integer values corresponding to its atomic number
@@ -20,14 +20,28 @@ std::map<std::string, int> SetElementMap(){
 }
 
 // Reads in 4 vectors from cell file: elements, x , y and z coords
-void ReadInXYZ(char* filename, std::vector<double>* xvect, std::vector<double>* yvect, std::vector<double>* zvect){
+void ReadInXYZ(char* filename, std::vector<double>* xvect, std::vector<double>* yvect, std::vector<double>* zvect, std::vector<double>* lats, bool pbc){
 	std::ifstream infile(filename);
 	// Initiate the map of elements (symbol-> atomic number)
 	std::map<std::string, int> elmap=SetElementMap();
-	// First two lines are handled seperately and are not to be stored
-	std::string skipline1, skipline2;
+	// First line handled seperately and not to be stored
+	std::string skipline1;
 	std::getline(infile, skipline1);
-	std::getline(infile, skipline2);
+	// If using periodic boundary conditions
+	if (pbc == 1) {
+	  // Read in a, b and c lattice parameters
+	  std::string name;
+	  double a, b, c;
+	  infile>>name>>a>>b>>c;
+	  (*lats).at(0) = a;
+	  (*lats).at(1) = b;
+	  (*lats).at(2) = c;
+	}
+	else {
+	  // Second line handled seperately and not to be stored
+	  std::string skipline2;
+	  std::getline(infile, skipline2);
+	}
 	// Store the molecule type and x, y, z positions
 	double x, y, z;
 	std::string type;
