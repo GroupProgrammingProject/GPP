001872f3aab2a81025778bc1499e29ab39158fde
alisevii <alise.virbule@gmail.com>
2015-03-02 22:36:14 +0000

    First version of creating dynamical matrix done, tested on graphene24, looked fairly symmetric, expected numerical errors

--- main.cpp
+++ main.cpp
@@ -19,10 +19,10 @@ int main(int argc, char* argv[]){
 	std::vector<double> lats(3);
 	// Read in types, 
 	std::vector<double> posx, posy, posz;
-	bool pbc = 0;
+	bool pbc = 1;
 	ReadInXYZ (argv[1],&posx, &posy, &posz, &lats, pbc);
 	// Number of atoms, number of orbitals, and number of MD steps
-	int n=posx.size(),norbs=4,nmd=1000,nprint=1;
+	int n=posx.size(),norbs=4,nmd=1,nprint=1;
 	// Velocities, reference postions, and vector neighbour list
 	std::vector<double> vx(n), vy(n), vz(n), refposx(n), refposy(n), refposz(n);
 	std::vector<int> nnear(n);
@@ -34,10 +34,10 @@ int main(int argc, char* argv[]){
 	// Matrix neighbour list
 	Eigen::MatrixXi inear(n,maxnn);
 	// Calculate distances
-	Eigen::MatrixXd modr(n,n);
-	Eigen::MatrixXd rx(n,n);
-	Eigen::MatrixXd ry(n,n);
-	Eigen::MatrixXd rz(n,n);
+	Eigen::MatrixXd modr(n,n), modr_0(n,n);
+	Eigen::MatrixXd rx(n,n), rx_0(n,n);
+	Eigen::MatrixXd ry(n,n), ry_0(n,n);
+	Eigen::MatrixXd rz(n,n), rz_0(n,n);
 	// Timestep, initial temperature, atomic mass, cut off and Verlet radii
 	double dt=1,T=500,Tf,m=12*1.0365e2,rc=2.6,rv=3,tmd,kb=1./11603;
 	GetDistances(&modr,&rx,&ry,&rz,&posx,&posy,&posz,&lats,rv,pbc);
@@ -47,57 +47,156 @@ int main(int argc, char* argv[]){
 	double ebs,erep,etot,ekin;
 	// Calculation of nearest neighbours:
 	NearestNeighbours(&inear,&nnear,&modr,rv);
-	// Calculation of initial velocities:
-	velocity(m,&vx,&vy,&vz,T);
-	// Initialisation of reference positions:
-	for(i=0;i<n;i++){
-	  refposx.at(i)=posx.at(i);
-	  refposy.at(i)=posy.at(i);
-	  refposz.at(i)=posz.at(i);
-	}
-
-
-	FILE *file=fopen("movie.txt","w");
-	fprintf(file,"%d\nC3 molecule\n",n);
-	for(i=0;i<n;i++){
-	  fprintf(file,"6  %f %f %f\n",posx.at(i),posy.at(i),posz.at(i));
-	}
 
 	// Starting TB	module: calculating energies
 	ebs=Hamiltonian(n,&modr,&rx,&ry,&rz,&eigvects,v);
 	//H_MD and eigvects have now also been populated
 	erep=Erep(&modr);	
-	ekin=3*(n-1)*kb*T/2;
-	etot=ebs+erep+ekin;
+/*
+	std::cout << "original geometry" << std::endl;
+	std::cout << "rx" << std:: endl << rx << std::endl;
+	std::cout << "ry" << std:: endl << ry << std::endl;
+	std::cout << "rz" << std:: endl << rz << std::endl;	
+	std::cout << "modr" << std:: endl << modr << std::endl;
+*/
+///	FORCES
+	std::vector<double> fx(n),fy(n),fz(n);
+	forces(n,norbs,rc,&rx,&ry,&rz,&modr,&eigvects,&nnear,&inear,&fx,&fy,&fz);//recalculate forces
+/*	std::cout << "original forces" << std::endl;
+	for(i=0;i<n;i++){
+		std::cout << "fx = " << fx[i] << std::endl;
+		std::cout << "fy = " << fy[i] << std::endl;
+		std::cout << "fz = " << fz[i] << std::endl << std::endl;
+	}
+*/
+	int J=0;					//atom to be displaced
+	int beta=0;				//direction of displacement
+	double diff=0.01;		//distance of displacement
 
-	FILE *en=fopen("energy.txt","w");
-	fprintf(en,"%f\t%f\t%f\t%f\t%f\n",0.0,ekin,ebs,erep,etot);
-	// MD cycle
-	for(i=1;i<nmd+1;i++){
-	  Tf=verlet(norbs,rc,rv,m,dt,&posx,&posy,&posz,&refposx,&refposy,&refposz,&vx,&vy,&vz,&eigvects,&nnear,&inear,&rx,&ry,&rz,&modr,ebs,&lats,pbc);
-	  ekin=3*(n-1)*kb*Tf/2;
-	  if(i%nprint==0){
-	    //H_MD and eigvects have now also been populated
-	    erep=Erep(&modr);
-	    etot=ebs+erep+ekin;
-	    tmd=i*dt;
-	    fprintf(en,"%f\t%f\t%f\t%f\t%f\n",tmd,ekin,ebs,erep,etot);
-	    fprintf(file,"%d\nC3 molecule\n",n);
-	    for(j=0;j<n;j++){
-	      fprintf(file,"6  %f %f %f\n",posx.at(j),posy.at(j),posz.at(j));
-	    }
-	  } 
+	//store original distances
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			rx_0(i,j)=rx(i,j);
+			ry_0(i,j)=ry(i,j);
+			rz_0(i,j)=rz(i,j);
+			modr_0(i,j)=modr(i,j);
+		}
 	}
 
-	// Starting TB	module: calculating energies
-	ebs=Hamiltonian(n,&modr,&rx,&ry,&rz,&eigvects,v);
-	//H_MD and eigvects have now also been populated
-	erep=Erep(&modr);
-	etot=ebs+erep+ekin;
+	Eigen::MatrixXd fr(3*n,3*n), fl(3*n,3*n);
+	Eigen::MatrixXd dynamicmat(3*n,3*n);
+	for(i=0;i<3*n;i++){
+		for(j=0;j<3*n;j++){
+			dynamicmat(i,j)=0;
+			fr(i,j)=0;
+			fl(i,j)=0;
+		}
+	}	
 
-	std::cout << "Ebs = " << ebs << std::endl;
-	std::cout << "Erep = " << erep << std::endl;
-	std::cout << "Etot = " << etot << std::endl;
+for(int l=0;l<2;l++){
+	if(l==0) { diff=-0.01; }			//fill up fr (moving atom J in beta direction by diff)
+	else if(l==1) { diff=0.01; } 		//fill up fl (move atom by -diff)
+	//Start loop over all n atoms
+	for(J=0;J<n;J++){
+		//Start loop over all three directions
+		for(beta=0;beta<3;beta++){
+			//revert all distances to original	
+			for(i=0;i<n;i++){
+				for(j=0;j<n;j++){
+					rx(i,j)=rx_0(i,j);
+					ry(i,j)=ry_0(i,j);
+					rz(i,j)=rz_0(i,j);
+					modr(i,j)=modr_0(i,j);
+				}
+			}
+
+/*		std::cout << "original geometry" << std::endl;
+		std::cout << "rx" << std:: endl << rx << std::endl;
+		std::cout << "ry" << std:: endl << ry << std::endl;
+		std::cout << "rz" << std:: endl << rz << std::endl;	
+		std::cout << "modr" << std:: endl << modr << std::endl;
+*/
+		//Update all distances (keep its own to 0)
+		for(i=0;i<n;i++){
+			if(i!=J){
+				if(beta==0){ 
+					rx(i,J)=rx(i,J)+diff;
+					rx(J,i)=-rx(i,J);
+				}
+				else if (beta==1){ 
+					ry(i,J)=ry(i,J)+diff;
+					ry(J,i)=-ry(i,J);
+				}
+				else if (beta==2){ 
+					rz(i,J)=rz(i,J)+diff; 
+					rz(J,i)=-rz(i,J);
+				}
+			}
+		}
+		for(i=0;i<n;i++){
+			if(i!=J){
+				modr(i,J)=sqrt(rx(i,J)*rx(i,J)+ry(i,J)*ry(i,J)+rz(i,J)*rz(i,J));
+			}
+		}
+/*
+		std::cout << "Moved atom " << J << " in " << beta << " direction" << std::endl;
+		std::cout << "rx" << std:: endl << rx << std::endl;
+		std::cout << "ry" << std:: endl << ry << std::endl;
+		std::cout << "rz" << std:: endl << rz << std::endl;	
+		std::cout << "modr" << std:: endl << modr << std::endl;
+*/
+ 		forces(n,norbs,rc,&rx,&ry,&rz,&modr,&eigvects,&nnear,&inear,&fx,&fy,&fz);//recalculate forces
+/*		std::cout << "new forces:" << std::endl;
+		for(i=0;i<n;i++){
+			std::cout << "fx = " << fx[i] << std::endl;
+			std::cout << "fy = " << fy[i] << std::endl;
+			std::cout << "fz = " << fz[i] << std::endl << std::endl;
+		}
+*/
+		for(i=0;i<n;i++){
+			if(l==0){
+				fr(3*i,3*J+beta)=fx[i];
+				fr(3*i+1,3*J+beta)=fy[i];
+				fr(3*i+2,3*J+beta)=fz[i];
+			}
+			else if (l==1){
+				fl(3*i,3*J+beta)=fx[i];
+				fl(3*i+1,3*J+beta)=fy[i];
+				fl(3*i+2,3*J+beta)=fz[i];
+
+			}
+		}
+//		std::cout << fr << std::endl;
+
+		}	//end beta loop
+	}	//end J loop
+}	//end l loop
+
+/*
+std::cout << "Final fr: " << std::endl;
+std::cout << fr << std::endl << std::endl;
+std::cout << "Final fl " << std::endl;
+std::cout << fl << std::endl;
+*/
+
+//Create dynamical matrix
+	for(i=0;i<3*n;i++){
+		for(j=0;j<3*n;j++){
+			dynamicmat(i,j)=(fr(i,j)-fl(i,j))/(2*fabs(diff));
+		}
+	}
+
+	std::cout << "Dynamic matrix:" << std::endl;
+	std::cout << dynamicmat << std::endl;
+
+/*	for(i=0;i<n;i++){
+		dynamicmat(3*i,3*J+beta)=fx[i];
+		dynamicmat(3*i+1,3*J+beta)=fy[i];
+		dynamicmat(3*i+2,3*J+beta)=fz[i];
+	}
+	std::cout << dynamicmat << std::endl;
+*/
+
 
 return 0;
 }
