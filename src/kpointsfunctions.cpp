deaa80c0c0518de29b308dc1a7859c23b94c7203
cja14 <cja14@ic.ac.uk>
2015-03-09 16:51:37 +0000

    Made kpointfunctions so that if only generating a single grid point in one dimension that it will be the gamma point

--- src/kpointsfunctions.cpp
+++ src/kpointsfunctions.cpp
@@ -15,15 +15,20 @@ void readinkpoints(char* filename, std::vector<std::vector<double> >* kpoints) {
 // Generate a user specified grid of kpoints (only for orthorhombic symmetries)
 void genkgrid(char* filename, std::vector<double>* lats, int kgrid[3], bool gamma) {
   std::ofstream outfile(filename);
+  bool gammas[3];
+  for (int i=0;i<3;i++) {
+	 gammas[i] = gamma;                           // Centre on gamma point in each dimension according to input parameter  
+	 if (kgrid[i] == 1) {gammas[i] = 1;}          // If only a single point needed make sure to centre on gamma
+  }
   std::cout << "kpoint grid " << kgrid[0] << " " << kgrid[1] << " " << kgrid[2] << std::endl;
   double kstep[3] = {2*M_PI/((*lats).at(0)*kgrid[0]), 2*M_PI/((*lats).at(1)*kgrid[1]), 2*M_PI/((*lats).at(2)*kgrid[2])}; 
   double kvec[3];
   for(double k0=-M_PI/(*lats).at(0);k0<M_PI/(*lats).at(0)-0.5*kstep[0];k0=k0+kstep[0]){
 	 for(double k1=-M_PI/(*lats).at(1);k1<M_PI/(*lats).at(1)-0.5*kstep[1];k1=k1+kstep[1]){
 		for(double k2=-M_PI/(*lats).at(2);k2<M_PI/(*lats).at(2)-0.5*kstep[2];k2=k2+kstep[2]){
-		  kvec[0] = k0 + gamma*0.5*kstep[0];
-		  kvec[1] = k1 + gamma*0.5*kstep[1];
-		  kvec[2] = k2 + gamma*0.5*kstep[2];
+		  kvec[0] = k0 + gammas[0]*0.5*kstep[0];
+		  kvec[1] = k1 + gammas[1]*0.5*kstep[1];
+		  kvec[2] = k2 + gammas[2]*0.5*kstep[2];
 		  outfile << kvec[0] << "\t" << kvec[1] << "\t" << kvec[2] << "\n";
 		}
 	 }
