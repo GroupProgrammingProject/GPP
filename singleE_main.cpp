08a423503a9c97719ec94e5f5c4fec63f30f4dc3
cja14 <cja14@ic.ac.uk>
2015-03-17 20:57:28 +0000

    Changed singleE to take additional run command

--- singleE_main.cpp
+++ singleE_main.cpp
@@ -13,34 +13,30 @@
 int main(int argc, char* argv[]){
 	// Read in parameter values passed by the run script with MODE="energy"
 	// xyzfilepath=argv[1], kptsfilepath=argv[3]
-	int norbs=atoi(argv[8]);
+   int norbs=atoi(argv[8]), maxnn=atoi(argv[9]);
 	double rv=atof(argv[6]), rc=atof(argv[7]);
 	bool kpts=atoi(argv[2]), pbc=atoi(argv[4]), v=atoi(argv[5]);
 
 	std::vector<double> TBparam(6);
-	TBparam[0]=atof(argv[9]);		// E_s
-	TBparam[1]=atof(argv[10]);		// E_p
-	TBparam[2]=atof(argv[11]);			// V_ss_sigma
-	TBparam[3]=atof(argv[12]);		//	V_sp_sigma
-	TBparam[4]=atof(argv[13]);		// V_pp_sigma
-	TBparam[5]=atof(argv[14]);		// V_pp_pi
+	TBparam[0]=atof(argv[10]);		// E_s
+	TBparam[1]=atof(argv[11]);		// E_p
+	TBparam[2]=atof(argv[12]);		// V_ss_sigma
+	TBparam[3]=atof(argv[13]);		//	V_sp_sigma
+	TBparam[4]=atof(argv[14]);		// V_pp_sigma
+	TBparam[5]=atof(argv[15]);		// V_pp_pi
 
-/*
-	//Turn kpoint sampling on or off
-	if(kpts==0){
-		if (argc<1){std::cout<<"You should append a file to the main object!"<<std::endl;}
-		if (argc!=2){std::cout<<"You should append one and only one xyz file to the main!!"<<std::endl;}
-	}
-	if(kpts==1){
-		if (argc<2){std::cout<<"You should append two files to the main object!"<<std::endl;}
-		if (argc!=3){std::cout<<"You should append one xyz and one .kpts file to the main!!"<<std::endl;}
-	}
-*/	
 	std::vector<double> lats(3), posx, posy, posz;
 	ReadInXYZ (argv[1], &posx, &posy, &posz, &lats, pbc);
 	int n=posx.size();
+	// Determine maximum number of nearest neighbours
+	if(n<maxnn){maxnn=n;}
+	// Matrix neighbour list
+	Eigen::MatrixXi inear(n,maxnn);
 	// Calculate distances
-	Eigen::MatrixXd modr(n,n), rx(n,n), ry(n,n), rz(n,n);
+	Eigen::MatrixXd modr(n,n);
+	Eigen::MatrixXd rx(n,n);
+	Eigen::MatrixXd ry(n,n);
+	Eigen::MatrixXd rz(n,n);
 	GetDistances(&modr,&rx,&ry,&rz,&posx,&posy,&posz,&lats,rv,pbc);
 	// Create data structures for energy calculations 
 	Eigen::MatrixXd eigvects(n*norbs,n*norbs);
