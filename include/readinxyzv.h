2b6a8ee46673feb452b06efd620b56a2c983f38c
rcharlton92@gmail.com <rcharlton92@gmail.com>
2015-03-17 22:06:54 +0000

    PBCs and velocity inputs fully incorporated into md_main.cpp. New function added to geometryinfo and readinxyz.cpp added (temporary).

---
+++ include/readinxyzv.h
@@ -0,0 +1,18 @@
+#ifndef READINXYZV_H
+#define READINXYZV_H
+
+// Read in from xyz file 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <vector>
+#include <stdlib.h>
+#include <sstream>
+#include "geometryinfo.h"
+//void pbcshift(std::vector<double>* x, std::vector<double>* y, std::vector<double>* z, std::vector<double>* lats);
+
+void ReadInXYZV (char* filename, std::vector<double>* x, std::vector<double>* y, std::vector<double>* z, std::vector<double>* vx, std::vector<double>* vy, std::vector<double>* vz,std::vector<double>* lats, bool pbc, std::vector<bool>* velspec);
+std::map<std::string, int> SetElementMap();
+
+#endif
--- main.cpp
+++ main.cpp
@@ -12,27 +12,25 @@
 #include <ctime>
 
 int main(int argc, char* argv[]){
-	// Read in the parameter values to run MD simulation
-	// The order in which they are passed is important, determined in run script
-	int nmd=atoi(argv[2]), nprint=atoi(argv[8]), norbs=atoi(argv[12]), maxnn=atoi(argv[13]);
-	double dt=atof(argv[3]), T=atof(argv[7]), rv=atof(argv[10]),rc=atof(argv[11]), nu=atof(argv[6]);
-	bool v=atoi(argv[9]), pbc=atoi(argv[4]), ander=atoi(argv[5]);
+	if (argc<1){std::cout<<"You should append a file to the main object!"<<std::endl;}
+	if (argc!=2){std::cout<<"You should append one and only one xyz file to the main!!"<<std::endl;}
 	
 	// Turn verbose mode (hamiltonian routine) on/off
-	bool renn;
+	bool v=0, renn;
 	int i,j;
 	std::vector<double> lats(3);
 	// Read in types, 
 	std::vector<double> posx, posy, posz, vxin, vyin, vzin;
+	bool pbc = 1, ander=0;
 	std::vector<bool> velspec;
-//	ReadInXYZ (argv[1],&posx, &posy, &posz, &lats, pbc);
-	ReadInXYZV (argv[1],&posx, &posy, &posz, &vxin, &vyin, &vzin, &lats, pbc, &velspec);
+	ReadInXYZV (argv[1],&posx, &posy, &posz, &vxin, &vyin, &vzin, &lats, pbc,&velspec);
 	// Number of atoms, number of orbitals, and number of MD steps
-	int n=posx.size();
+	int n=posx.size(),norbs=4,nmd=1000,nprint=1;
 	// Velocities, reference postions, and vector neighbour list
 	std::vector<double> vx(n), vy(n), vz(n), refposx(n), refposy(n), refposz(n), fx(n), fy(n), fz(n);
 	std::vector<int> nnear(n);
 	// Determine maximum number of nearest neighbours
+	int maxnn=100;
 	if(n<maxnn){maxnn=n;}
 	// Matrix neighbour list
 	Eigen::MatrixXi inear(n,maxnn);
@@ -42,20 +40,9 @@ int main(int argc, char* argv[]){
 	Eigen::MatrixXd ry(n,n);
 	Eigen::MatrixXd rz(n,n);
 	// Timestep, initial temperature, atomic mass, cut off and Verlet radii
-	double Tf,m=12*1.0365e2,tmd,kb=1./11603;
+	double dt=1,T=100,Tf,m=12*1.0365e2,rc=2.6,rv=3,tmd,kb=1./11603,nu=0.5;
 	GetDistances(&modr,&rx,&ry,&rz,&posx,&posy,&posz,&lats,rv,pbc);
 	// Create empty arrays needed for MD
-
-	std::vector<double> TBparam(6);
-	// In final version, TB params will be read in from input file
-	TBparam[0]=-2.99;		// E_s
-	TBparam[1]=3.71;		// E_p
-	TBparam[2]=-5;			// V_ss_sigma
-	TBparam[3]=4.7;		//	V_sp_sigma
-	TBparam[4]=5.5;		// V_pp_sigma
-	TBparam[5]=-1.55;		// V_pp_pi
-
-	//Matrix for eigenvectors
 	Eigen::MatrixXd eigvects(norbs*n,norbs*n);
 	// Energies from TB model
 	double ebs,erep,etot,ekin;
@@ -77,13 +64,14 @@ int main(int argc, char* argv[]){
 	  refposy.at(i)=posy.at(i);
 	  refposz.at(i)=posz.at(i);
 	}
+
 	FILE *file=fopen("movie.txt","w");
-	fprintf(file,"%d\nC%d\n",n,n);
+	fprintf(file,"%d\nC3 molecule\n",n);
 	for(i=0;i<n;i++){
-	  fprintf(file,"6  %f %f %f \n",posx.at(i),posy.at(i),posz.at(i));
+	  fprintf(file,"6  %f %f %f %f %f %f\n",posx.at(i),posy.at(i),posz.at(i),vx.at(i),vy.at(i),vz.at(i));
 	}
 	// Starting TB	module: calculating energies
-	ebs=Hamiltonian(n,norbs,&TBparam,&modr,&rx,&ry,&rz,&eigvects,v);
+	ebs=Hamiltonian(n,&modr,&rx,&ry,&rz,&eigvects,v);
 	//H_MD and eigvects have now also been populated
 	erep=Erep(&modr);	
 	ekin=3*(n-1)*kb*T/2;
@@ -93,35 +81,38 @@ int main(int argc, char* argv[]){
 	fprintf(en,"%f\t%f\t%f\t%f\t%f\t%f\n",0.0,T,ekin,ebs,erep,etot);
 	double xi1=0,xi2=0,vxi1=0,vxi2=0,q1=1,q2=1;
 	// MD cycle
-	for(i=0; i<nmd; i++){
-	  Tf=verlet(norbs,rc,rv,m,dt,&posx,&posy,&posz,&refposx,&refposy,&refposz,&vx,&vy,&vz,&eigvects,&nnear,&inear,&rx,&ry,&rz,&modr,ebs,&lats,pbc,T,nu,ander,&TBparam);
+	for(i=1;i<nmd+1;i++){
+		if(i*10%nmd==0){
+			std::cout << i*10/nmd << "0% completed" << std::endl;}
+	  forces(n,norbs,rc,&rx,&ry,&rz,&modr,&eigvects,&nnear,&inear,&fx,&fy,&fz);
+	  Tf=verlet(norbs,rc,rv,m,dt,&posx,&posy,&posz,&refposx,&refposy,&refposz,&vx,&vy,&vz,&eigvects,&nnear,&inear,&rx,&ry,&rz,&modr,ebs,&lats,pbc,T,nu,ander);
+	  //canonical ensemble function
 	  ekin=3*(n-1)*kb*Tf/2;
 	  for(int k=0; k<n; k++){
-			fprintf(f,"%f\t%f\t%f\t\n",fx.at(k),fy.at(k),fz.at(k));
+				 fprintf(f,"%f\t%f\t%f\t\n",fx.at(k),fy.at(k),fz.at(k));
 	  }
 	  if(i%nprint==0){
-	    //H_MD and eigvects have now also been populated
-	    erep=Erep(&modr);
-	    etot=ebs+erep+ekin;
-	    tmd=i*dt;
-	    fprintf(en,"%f\t%f\t%f\t%f\t%f\t%f\n",tmd,Tf,ekin,ebs,erep,etot);
-	    fprintf(file,"%d\nC%d molecule\n",n,n);
-	    for(j=0;j<n;j++){
-	      fprintf(file,"6  %f %f %f\n",posx.at(j),posy.at(j),posz.at(j));
-	    }
+			//H_MD and eigvects have now also been populated
+	    	erep=Erep(&modr);
+	    	etot=ebs+erep+ekin;
+	    	tmd=i*dt;
+	    	fprintf(en,"%f\t%f\t%f\t%f\t%f\t%f\n",tmd,Tf,ekin,ebs,erep,etot);
+			if(pbc==1){fprintf(file,"%d\nC%d.xyz %f %f %f\n",n,n,lats.at(0),lats.at(1),lats.at(2));}
+			else{fprintf(file,"%d\nC%d.xyz\n",n,n);}
+	    	for(j=0;j<n;j++){
+	      	fprintf(file,"6  %f %f %f %f %f %f\n",posx.at(j),posy.at(j),posz.at(j),vx.at(j), vy.at(j), vz.at(j));
+			}
 	  } 
-	  if(i*10%nmd==0){
-	    std::cout << i*10/nmd << "0 % completed" << std::endl;
-	  }
 	}
+	FILE *fin=fopen("final.xyz","w");
+	if(pbc==1){fprintf(fin,"%d\nC%d.xyz %f %f %f\n",n,n,lats.at(0),lats.at(1),lats.at(2));}
+	else{fprintf(fin,"%d\nC%d.xyz\n",n,n);}
 	//Output final positions and velocities to a .xyz file for future simulations
-	FILE *rel=fopen("final.xyz","w");
-	fprintf(rel,"%d\nC%d molecule\n",n,n);
 	for(j=0;j<n;j++){
-		fprintf(rel,"6  %f %f %f %f %f %f\n",posx.at(j),posy.at(j),posz.at(j),vx.at(j),vy.at(j),vz.at(j));
+		fprintf(fin,"6  %f %f %f %f %f %f\n",posx.at(j),posy.at(j),posz.at(j),vx.at(j), vy.at(j), vz.at(j));
 	}
 	// Starting TB	module: calculating energies
-	ebs=Hamiltonian(n,norbs,&TBparam,&modr,&rx,&ry,&rz,&eigvects,v);
+	ebs=Hamiltonian(n,&modr,&rx,&ry,&rz,&eigvects,v);
 	//H_MD and eigvects have now also been populated
 	erep=Erep(&modr);
 	etot=ebs+erep+ekin;
@@ -129,6 +120,5 @@ int main(int argc, char* argv[]){
 	std::cout << "Ebs = " << ebs << std::endl;
 	std::cout << "Erep = " << erep << std::endl;
 	std::cout << "Etot = " << etot << std::endl;
-
-return 0;
+	return 0;
 }
